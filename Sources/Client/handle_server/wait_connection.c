d396586962fcf711f37d1e88e1b847e7085d3a63
Fradet Julien <fradet_j@localhost.localdomain>
2018-06-25 17:31:57 +0200

    Wait until someone try to be connected to a server

---
+++ Sources/Client/handle_server/wait_connection.c
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2018
+** PSU_myirc_2017
+** File description:
+** wait_connection.c
+*/
+
+#include "../../../Includes/client.h"
+
+int		wait_connection(t_client *Newclient)
+{
+	int	error = 0;
+
+	while (Newclient->socket->fd == -1) {
+		if (read_instruction(0, Newclient) == -1) {
+			dprintf(2, "Can't read on this fd\n");
+			return (-1);
+		}
+		error = is_writearguments(Newclient);
+		if (error == 0) {
+			error = connection_server(Newclient);
+		}
+	}
+	return (error);
+}
