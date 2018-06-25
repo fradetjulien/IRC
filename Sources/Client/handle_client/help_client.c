b0ef82de467ebff51772191f053993038f18ea21
Fradet Julien <fradet_j@localhost.localdomain>
2018-06-25 17:04:04 +0200

    Help client when usage is not good

---
+++ Sources/Client/handle_client/help_client.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2018
+** PSU_myirc_2017
+** File description:
+** help_client.c
+*/
+
+#include "../../../Includes/client.h"
+
+void		help_client(char *binary)
+{
+	dprintf(1, "USAGE : %s\n", binary);
+}
