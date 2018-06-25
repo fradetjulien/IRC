7c340f0920235c63d0f35abb71d2c62751d1f392
Fradet Julien <fradet_j@localhost.localdomain>
2018-06-25 16:39:37 +0200

    Beginning of the development of the client part

---
+++ Includes/client.h
@@ -0,0 +1,65 @@
+/*
+** EPITECH PROJECT, 2018
+** PSU_myirc_2017
+** File description:
+** client.h
+*/
+
+#ifndef CLIENT_H_
+# define CLIENT_H_
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet.ip.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+# define LOCALHOST	"127.0.0.1"
+
+typedef struct			s_socket
+{
+	int			fd;
+	struct sockaddr_in	s;
+	socklen_t		len;
+}t_socket;
+
+typedef struct			s_client
+{
+	struct protoent		*protocol;
+	char			**command;
+	char			*port;
+	char			*host;
+	char			*answer;
+	fd_set			*read;
+	fd_set			*write;
+	t_socket		*socket;
+}t_client;
+
+/* Handle Client */
+int				init_client(t_client *Newclient);
+int				launch_client(t_client *Newclient);
+void				help_client(char *binary);
+void				display_command(void);
+
+/* Handle FD_SET */
+void		init_fds(int fd, fd_set *r, fd_set *w, struct timeval *t);
+int				check_fds(t_client *NewClient);
+
+/* Handle Server */
+int				wait_connection(t_client *Newclient);
+int				connection_server(t_client *Newclient);
+
+/* Handle Instructions */
+int				read_instruction(int fd, t_client *Newclient);
+
+/* Handle Arguments */
+int				is_writearguments(t_client *Newclient);
+int				is_hostname(char *host);
+int				is_port(char *port);
+
+#endif /* !CLIENT_H_ */
---
+++ Sources/Client/handle_client/client.c
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2018
+** PSU_myirc_2017
+** File description:
+** client.c
+*/
+
+#include "Client.h"
+
+int			main(int ac, char **av)
+{
+	t_client	Newclient;
+
+	if (ac != 1 || strcmp(av[0], "./client") != 0) {
+		help_client("./client");
+		return (-1);
+	}
+	if (init_client(&Newclient) == -1)
+		return (-1);
+	launch_client(Newclient);
+	return (0);
+}
